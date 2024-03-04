5d9375e34d65810f10701076341df04e81cb0f43
Saumya Ranjan <58022610+technicals-123@users.noreply.github.com>
2024-03-04 15:18:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "example" {
   location = "Central India"
 }
 resource "azurerm_storage_account" "example1" {
-  name                     = "storageaccountforfiletwo"
+  name                     = "storageaccountforfilefour"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
