2b0a9c8f540e3c316d40b0ba53687c306824b219
Saumya Ranjan <58022610+technicals-123@users.noreply.github.com>
2024-03-04 16:08:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,5 +5,5 @@ module "azure_resources" {
   resource_group_name        = "example_rg"
   resource_group_location    = "Central India"
   storage_account_name       = "storageaccounttriall"
-  blob_container_name        = "example_container"
+  blob_container_name        = "storageaccounttriallcontainer"
 }
