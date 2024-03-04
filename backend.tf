33c9e24218f0078b218a2248c51971862c7da2b0
Saumya Ranjan <58022610+technicals-123@users.noreply.github.com>
2024-03-04 17:44:15 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,5 +1,6 @@
 terraform {
   backend "azurerm" {
+    resource_group_name = "new-resource"
     storage_account_name = "mystorageaccounti"
     container_name       = "statefile"
     key                  = "terraform.tfstate"
