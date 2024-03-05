aaf2b9a9b0e1c6332e8e739c7310ae4c7153706f
Saumya Ranjan <58022610+technicals-123@users.noreply.github.com>
2024-03-05 20:36:04 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,23 @@
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
+  default=   "example_rg1"
 }
 
 variable "resource_group_location" {
   description = "Location for the resource group"
   type        = string
+default= "Central India"
 }
 
 variable "storage_account_name" {
   description = "Name of the storage account"
   type        = string
+default= "storageaccounttrialll"
 }
 
 variable "blob_container_name" {
   description = "Name of the blob container"
   type        = string
+ default= "storageaccounttrialllcontainer"
 }
