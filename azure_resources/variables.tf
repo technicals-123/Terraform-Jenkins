e6284d7ea8cd66a408812b10f96c7cd689972f60
Saumya Ranjan <58022610+technicals-123@users.noreply.github.com>
2024-03-05 20:59:53 +0530

    Create variables.tf

---
+++ azure_resources/variables.tf
@@ -0,0 +1,18 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "resource_group_location" {
+  description = "Location of the resource group"
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "Name of the storage account"
+  type        = string
+}
+variable "blob_container_name" {
+  description = "Name of the Blob container"
+  type        = string
+}
