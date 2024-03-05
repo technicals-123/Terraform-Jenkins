399304fa310630aea73b603b273b9213e49feeb9
Saumya Ranjan <58022610+technicals-123@users.noreply.github.com>
2024-03-05 11:19:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ module "azure_resources" {
   source = "./azure_resources"  # Path to your module directory
 
   # Input variables for the module
-  resource_group_name        = "example_rg"
-  resource_group_location    = "Central India"
-  storage_account_name       = "storageaccounttriall"
-  blob_container_name        = "storageaccounttriallcontainer"
+   resource_group_name    = var.resource_group_name
+  resource_group_location = var.resource_group_location
+  storage_account_name    = var.storage_account_name
+  blob_container_name     = var.blob_container_name
 }
